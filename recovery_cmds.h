LineageOS/android_bootable_recovery

recovery: Get a proper shell environment in recovery
 * ADB support
 * Toybox applets
 * mksh
 * Set time

Change-Id: I80b0e2aa5eb7142eaa9f157709f4e029077d8dfa

recovery_cmds.h
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _RECOVERY_CMDS_H
+#define _RECOVERY_CMDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int reboot_main(int argc, char **argv);
+int poweroff_main(int argc, char **argv);
+int start_main(int argc, char **argv);
+int stop_main(int argc, char **argv);
+int mksh_main(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

PixysOS/bootable_recovery

 * ADB shell support with mksh
 * Shell reboot/poweroff

recovery_cmds.h
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _RECOVERY_CMDS_H
+#define _RECOVERY_CMDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int reboot_main(int argc, char** argv);
+int poweroff_main(int argc, char** argv);
+int start_main(int argc, char** argv);
+int stop_main(int argc, char** argv);
+int mksh_main(int argc, char** argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif