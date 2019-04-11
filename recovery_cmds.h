LineageOS/android_bootable_recovery

recovery: Enable gunzip/gzip/unzip/zip commands
Change-Id: I938dc855742d4e1a5ba0b09fd74851b0d155f0a1

recovery_cmds.h
@@ -21,6 +21,9 @@
 extern "C" {
 #endif
 
+int pigz_main(int argc, char **argv);
+int miniunz_main(int argc, char **argv);
+int minizip_main(int argc, char **argv);
 int reboot_main(int argc, char **argv);
 int poweroff_main(int argc, char **argv);
 int start_main(int argc, char **argv);

PixysOS/bootable_recovery

recovery_cmds.h
@@ -22,6 +22,9 @@
 extern "C" {
 #endif
 
+int pigz_main(int argc, char** argv);
+int miniunz_main(int argc, char** argv);
+int minizip_main(int argc, char** argv);
 int reboot_main(int argc, char** argv);
 int poweroff_main(int argc, char** argv);
 int start_main(int argc, char** argv);